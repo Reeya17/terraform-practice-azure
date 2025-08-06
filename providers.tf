30da4d3d0bf6a422c7ab56ce15eda200ed8c6cf8
Riya Rani <89979996+Reeya17@users.noreply.github.com>
2025-08-06 15:35:30 +0200

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,1 +1,37 @@
+terraform {
+    required_providers {
+        azurerm = {
+            alias   = "az"
+            source  = "hashicorp/azurerm"
+            version = "3.0.0"
+        }
+        aws = {
+            alias   = "aw"
+            source  = "hashicorp/aws"
+            version = "3.0.0"
+        } 
+    }
+}
+
+provider "aws" {
+  alias  = "aws-us-west-2"
+  region = "us-west-2"
+  
+}
+provider "aws" {
+  alias  = "aws-us-east-1"
+  region = "us-east-1"
+  
+}
+
+
+#we cannot use the multi-location using provider block as it is not supported 
+#in the current version of Terraform in azurerm provider
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
 
